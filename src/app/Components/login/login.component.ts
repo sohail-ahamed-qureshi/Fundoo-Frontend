fbd8248686e3f7aae4eccde972e17796827fb91c
sohail <sohailqureshi82@gmail.com>
2021-08-16 08:49:40 +0530

    [soahil]:recycle:Added validations for login page

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  submitted = false;
+  constructor(private formbuilder: FormBuilder) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.loginForm = this.formbuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
   }
 
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  OnSignin() {
+    // method on sign in
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+      return;
+    }
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.loginForm.value, null, 4));
+  }
 }
